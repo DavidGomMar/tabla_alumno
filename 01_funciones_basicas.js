4678624eb9ee2a60ac046a3dd720467f510b0476
DavidGonzalez <daesda220417@gmail.com>
2021-11-05 17:49:35 -0600

    Funciones basicas

---
+++ 01_funciones_basicas.js
@@ -0,0 +1,13 @@
+function saludar() {
+    console.log("Hola, mundo");
+}
+
+saludar();
+
+// Función anónima
+const saludoEnIngles = function () {
+    console.log("Hello, world");
+};
+
+saludoEnIngles();
+
